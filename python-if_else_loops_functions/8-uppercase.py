bd2621dac4ab67d991917afc1342e12e3eeacf93
Tristan <tristan.latuilliere@gmail.com>
2025-09-02 15:33:53 +0200

    .format added

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -5,6 +5,9 @@ def uppercase(str):
     for c in str:
         if ord(c) > 96 and ord(c) < 123:
             upper_ASCII = ord(c) - 32
-            print(chr(upper_ASCII), end="")
+            print("{}".format(chr(upper_ASCII)), end="")
         else:
             print(c, end="")
+
+
+uppercase("Bonjour")
