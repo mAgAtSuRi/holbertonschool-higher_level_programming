85d0add3367f11af19da90af06da7de175fb9c1b
Tristan <tristan.latuilliere@gmail.com>
2025-09-02 11:38:44 +0200

    new line added

--- python-hello_world/test.py
+++ python-hello_world/test.py
@@ -1,1 +1,4 @@
-input("What's your name ?\n")
+print("Welcome to the brand name generator")
+city = input("What's the name of the city you grew in ?\n")
+pet_name = input("What's your pet name ?\n")
+print(f"Your band name could be {city} {pet_name}")
