cb772f3d361ef51fa9befee83cefa7ca6637dd56
Tristan <tristan.latuilliere@gmail.com>
2025-09-02 16:42:08 +0200

    second try

--- python-if_else_loops_functions/101-remove_char_at.py
+++ python-if_else_loops_functions/101-remove_char_at.py
@@ -6,6 +6,11 @@ def remove_char_at(str, n):
     new_str = ""
     for c in str:
         if count == n:
+            count += 1
             continue
         new_str += c
+        count += 1
     return new_str
+
+
+print(remove_char_at("012345", 1))
