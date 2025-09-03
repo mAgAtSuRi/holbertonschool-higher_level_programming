248107b9bdd4c836d3403bfd750e7586750afedf
mAgAtSuRi <tristan.latuilliere@gmail.com>
2025-09-03 14:55:02 +0200

    arg num -1

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -10,6 +10,6 @@ if __name__ == "__main__":
         print("1 argument:")
         print("1: {}".format(sys.argv[1]))
     else:
-        print("{} arguments:".format(arg_num))
+        print("{} arguments:".format(arg_num - 1))
         for i in range(1, arg_num):
             print("{}: {}".format(i, sys.argv[i]))
