4752c9f87043aedea27827feb1302bafb0b2edac
mAgAtSuRi <tristan.latuilliere@gmail.com>
2025-09-08 11:23:37 +0200

    8 ft

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 
+
 def multiple_returns(sentence):
-	if sentence == "":
-		return (len(sentence), "")
-	return (len(sentence), sentence[0])
-sentence = "At school, I learnt C!"
-length, first = multiple_returns(sentence)
-print("Length: {:d} - First character: {}".format(length, first))
+    if sentence == "":
+        return (len(sentence), "")
+    return (len(sentence), sentence[0])
