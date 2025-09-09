60be0198a3ed8a50473c46b9c9958c60530a2571
Tristan <tristan.latuilliere@gmail.com>
2025-09-09 15:57:14 +0200

    new dic created

--- python-more_data_structures/9-multiply_by_2.py
+++ python-more_data_structures/9-multiply_by_2.py
@@ -2,7 +2,7 @@
 
 
 def multiply_by_2(a_dictionary):
-    # return ((a_dictionary[key] * 2) for key in a_dictionary)
-    for key in a_dictionary:
-        a_dictionary[key] = a_dictionary[key] * 2
-    return a_dictionary
+    new_dic = {}
+    for key, value in a_dictionary:
+        new_dic[key] = value * 2
+    return new_dic
