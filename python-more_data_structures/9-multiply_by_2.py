34fab116b1806c2bb9f3fa96fe1647072a13c5a4
Tristan <tristan.latuilliere@gmail.com>
2025-09-09 15:49:47 +0200

    9 ft

---
+++ python-more_data_structures/9-multiply_by_2.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+
+def multiply_by_2(a_dictionary):
+    return (a_dictionary[key] * 2 for key in a_dictionary)
+    # for key in a_dictionary:
+    # 	a_dictionary[key] = a_dictionary[key] * 2
