17c8fb1e279e7e619e001c289888d81e8e4fb227
Tristan <tristan.latuilliere@gmail.com>
2025-09-09 15:46:24 +0200

    formatted

--- python-more_data_structures/8-simple_delete.py
+++ python-more_data_structures/8-simple_delete.py
@@ -6,6 +6,3 @@ def simple_delete(a_dictionary, key=""):
         return a_dictionary
     del a_dictionary[key]
     return a_dictionary
-
-a_dictionary = { 'language': "C", 'Number': 89, 'track': "Low", 'ids': [1, 2, 3] }
-simple_delete(a_dictionary, key='h')
