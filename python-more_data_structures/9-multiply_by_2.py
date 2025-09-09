cd206d848c7d57ed9d8184e537eef467d8cb3be7
Tristan <tristan.latuilliere@gmail.com>
2025-09-09 16:01:45 +0200

    added dic.items()

--- python-more_data_structures/9-multiply_by_2.py
+++ python-more_data_structures/9-multiply_by_2.py
@@ -2,7 +2,8 @@
 
 
 def multiply_by_2(a_dictionary):
-    new_dic = {}
-    for key, value in a_dictionary:
-        new_dic[key] = value * 2
-    return new_dic
+    return {key: value * 2 for key, value in a_dictionary.items()}
+    # new_dic = {}
+    # for key, value in a_dictionary.items():
+    #     new_dic[key] = value * 2
+    # return new_dic
