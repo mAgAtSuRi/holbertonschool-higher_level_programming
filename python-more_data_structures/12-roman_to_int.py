e29eb9b9bcfe42748d3ef512a67c2a8f133f2ef2
Tristan <tristan.latuilliere@gmail.com>
2025-09-09 16:39:36 +0200

    new try

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -6,6 +6,12 @@ def roman_to_int(roman_string):
         return 0
     roman_dic = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
     result = 0
-    for letter in roman_string:
-        result += roman_dic[letter]
+    for i in range(len(roman_string) - 1):
+        if roman_dic[roman_string[i]] < roman_dic[roman_string[i + 1]]:
+            result -= roman_dic[roman_string[i]]
+        else:
+            result += roman_dic[roman_string[i]]
+    result += roman_dic[roman_string[len(roman_string) - 1]]
     return result
+
+print(roman_to_int('XL'))
