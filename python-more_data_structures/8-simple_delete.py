2078385b42e44b5fd774dd5631f4254cc0eda0fb
Tristan <tristan.latuilliere@gmail.com>
2025-09-09 15:46:06 +0200

    added check if key is in dic

--- python-more_data_structures/8-simple_delete.py
+++ python-more_data_structures/8-simple_delete.py
@@ -2,5 +2,10 @@
 
 
 def simple_delete(a_dictionary, key=""):
+    if key not in a_dictionary:
+        return a_dictionary
     del a_dictionary[key]
     return a_dictionary
+
+a_dictionary = { 'language': "C", 'Number': 89, 'track': "Low", 'ids': [1, 2, 3] }
+simple_delete(a_dictionary, key='h')
