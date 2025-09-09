77e6ce98e67f1af0a921a4ce31b1ec69decc73be
Tristan <tristan.latuilliere@gmail.com>
2025-09-09 16:13:18 +0200

    2 args a max

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -2,6 +2,6 @@
 
 
 def best_score(a_dictionary):
-    if a_dictionary is None:
+    if not a_dictionary:
         return None
-    return max(a_dictionary.values())
+    return max(a_dictionary, key=a_dictionary.get)
