86db0138ef47dc7096c890d1488f8569ca5daecd
Tristan <tristan.latuilliere@gmail.com>
2025-09-09 14:27:56 +0200

    ft 0

---
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def square_matrix_simple(matrix=[]):
+	square_matrix = [row[:] for row in matrix]
+	for row in square_matrix:
+		for i in range(len(row)):
+			row[i] = row[i] ** 2
+	return square_matrix
+# with comprehension:
+# return [[element ** 2 for element in row] for row in matrix]
