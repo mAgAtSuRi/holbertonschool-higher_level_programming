e05872d52ad3b75597d066029b2c491a28a2a314
Tristan <tristan.latuilliere@gmail.com>
2025-09-09 16:40:27 +0200

    formatted

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -4,7 +4,8 @@
 def roman_to_int(roman_string):
     if type(roman_string) is not str or not roman_string:
         return 0
-    roman_dic = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+    roman_dic = {"I": 1, "V": 5, "X": 10, "L": 50,
+                 "C": 100, "D": 500, "M": 1000}
     result = 0
     for i in range(len(roman_string) - 1):
         if roman_dic[roman_string[i]] < roman_dic[roman_string[i + 1]]:
