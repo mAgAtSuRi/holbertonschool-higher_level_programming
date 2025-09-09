d58dc52d9247ab8d552e8b1dc20a48da61b53cbf
Tristan <tristan.latuilliere@gmail.com>
2025-09-09 14:45:51 +0200

    other technique

--- python-more_data_structures/2-uniq_add.py
+++ python-more_data_structures/2-uniq_add.py
@@ -2,11 +2,14 @@
 
 
 def uniq_add(my_list=[]):
-    uniqu_list = []
-    for num in my_list:
-        if num not in uniqu_list:
-            uniqu_list.append(num)
-    sum = 0
-    for num in uniqu_list:
-        sum += num
-    return sum
+    return sum(set(my_list))
+
+# my technique:
+# uniqu_list = []
+#     for num in my_list:
+#         if num not in uniqu_list:
+#             uniqu_list.append(num)
+#     sum = 0
+#     for num in uniqu_list:
+#         sum += num
+#     return sum
