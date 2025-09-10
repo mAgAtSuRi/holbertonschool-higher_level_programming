d2edd7096c6cbd297e1e67bf93a9e95ea8f47594
mAgAtSuRi <tristan.latuilliere@gmail.com>
2025-09-10 12:27:40 +0200

    append(0) added

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -7,6 +7,7 @@ def list_division(my_list_1, my_list_2, list_length):
         try:
             new_list.append(my_list_1[i] / my_list_2[i])
         except (TypeError, ValueError):
+            new_list.append(0)
             print("wrong type")
         except ZeroDivisionError:
             new_list.append(0)
