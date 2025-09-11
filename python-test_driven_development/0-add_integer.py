4288bc773071c82e41ef22fbce79b23db061c7c3
Tristan <tristan.latuilliere@gmail.com>
2025-09-11 16:28:58 +0200

    added try except for big float

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,10 +1,3 @@
-#!/usr/bin/python3
-
-"""
-Adds 2 integers and check if it's doable
-"""
-
-
 def add_integer(a, b=98):
     """
     Adds two numbers after checking if they are integers or floats.
@@ -19,9 +12,13 @@ def add_integer(a, b=98):
 
     Raises:
         TypeError: if a or b are not int or float
+        OverflowError: if a or b is too large to convert to int
     """
-    if type(a) not in [int, float]:
+    if type(a) not in (int, float):
         raise TypeError("a must be an integer")
-    if type(b) not in [int, float]:
+    if type(b) not in (int, float):
         raise TypeError("b must be an integer")
-    return int(a) + int(b)
+    try:
+        return int(a) + int(b)
+    except OverflowError:
+        raise OverflowError("number too large to convert to integer")
