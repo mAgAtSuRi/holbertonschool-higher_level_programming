fde6efc5658e0abc08f83b67aa46cdb86dfe6796
Tristan <tristan.latuilliere@gmail.com>
2025-09-11 16:32:11 +0200

    go back

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-Adds 2 integers and check if its doable
+Adds 2 integers and check if it's doable
 """
 
 
@@ -18,13 +18,9 @@ def add_integer(a, b=98):
 
     Raises:
         TypeError: if a or b are not int or float
-        OverflowError: if a or b is too large to convert to int
     """
     if type(a) not in (int, float):
         raise TypeError("a must be an integer")
     if type(b) not in (int, float):
         raise TypeError("b must be an integer")
-    try:
-        return int(a) + int(b)
-    except OverflowError:
-        raise OverflowError("number too large to convert to integer")
+    return int(a) + int(b)
