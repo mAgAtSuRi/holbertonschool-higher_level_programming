dfee99414b4ac33f7e8bc93859cd872179b1ce09
mAgAtSuRi <tristan.latuilliere@gmail.com>
2025-09-12 11:43:55 +0200

    0 ft

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -5,20 +5,14 @@ prints name and first name
 first_name: first name to print
 last_name: last_name to print
 """
+
+
 def say_my_name(first_name, last_name=""):
-	"""
-	prints first name and name
-	"""
-	if type(first_name) is not str:
-		raise TypeError("first_name must be a string")
-	if type(last_name) is not str:
-		raise TypeError("last_name must be a string")
-	print(f"My name is {first_name} {last_name}")
-
-
-say_my_name("Walter", "White", "black")
-say_my_name("Bob")
-try:
-    say_my_name(12, "White")
-except Exception as e:
-    print(e)
+    """
+    prints first name and name
+    """
+    if type(first_name) is not str:
+        raise TypeError("first_name must be a string")
+    if type(last_name) is not str:
+        raise TypeError("last_name must be a string")
+    print(f"My name is {first_name} {last_name}")
