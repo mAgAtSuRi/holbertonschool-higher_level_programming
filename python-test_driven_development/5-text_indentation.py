39fb991ee3585aed925ba6a14f775409ec99408e
mAgAtSuRi <tristan.latuilliere@gmail.com>
2025-09-12 14:37:43 +0200

    reverse

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -18,10 +18,10 @@ def text_indentation(text):
     while i < len(text):
         if text[i] in [".", "?", ":"]:
             new_text += text[i] + "\n\n"
-            i += 1
-            while text[i] == " " and i < len(text):
+            while text[i + 1] == " " and i + 1 < len(text):
                 i += 1
         else:
             new_text += text[i]
         i += 1
     print(new_text, end="")
+text_indentation("Hello. How are you ? My name is: John")
