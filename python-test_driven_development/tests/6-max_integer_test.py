93738f472d4d76d8eb186cbda7001a6595aed97a
mAgAtSuRi <tristan.latuilliere@gmail.com>
2025-09-12 15:12:56 +0200

    test_ added

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -8,24 +8,24 @@ class TestMaxInteger(unittest.TestCase):
     def test_ordered_list(self):
         self.assertEqual(max_integer([1, 2, 3, 4]), 4)
 
-    def max_beginning(self):
+    def test_max_beginning(self):
         self.assertEqual(max_integer([4, 2, 1, 3]), 4)
 
-    def max_middle(self):
+    def test_max_middle(self):
         self.assertEqual(max_integer([4, 2, 10, 1, 3]), 10)
 
-    def empty_list(self):
+    def test_empty_list(self):
         self.assertEqual(max_integer(), None)
 
-    def one_negative_number(self):
+    def test_one_negative_number(self):
         self.assertEqual(max_integer([10, -5, 2]), 10)
 
-    def negative_number(self):
+    def test_negative_number(self):
         self.assertEqual(max_integer([-10, -1, -3]), -1)
 
-    def multiple_max(self):
+    def test_multiple_max(self):
         self.assertEqual(max_integer([1, 2, 2, 0]), 2)
 
-    def error_string(self):
+    def test_error_string(self):
         with self.assertRaises(TypeError):
             max_integer([1, 2, "word"])
