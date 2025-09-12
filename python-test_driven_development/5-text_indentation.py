e77139b15d3e759b59af3b96d7663e68e44cc8f5
mAgAtSuRi <tristan.latuilliere@gmail.com>
2025-09-12 14:12:29 +0200

    5 ft

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -1,1 +1,22 @@
-#§
+#!/usr/bin/python3
+
+"""
+prints a text with 2 new lines after each of these characters: ., ? and :
+text: text to print
+"""
+
+
+def text_indentation(text):
+    """
+    prints a text with 2 new lines after each of these characters: ., ? and :
+    """
+    if type(text) is not str:
+        raise TypeError("text must be a string")
+    new_text = ""
+    for i in range(len(text)):
+        if text[i] in [".", "?", ":"]:
+            new_text += text[i] + "\n\n"
+        else:
+            new_text += text[i]
+    print(new_text)
+text_indentation('f', 'erer')
