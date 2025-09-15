0d614dd45be076044c3eafbea0772feca7b7d868
mAgAtSuRi <tristan.latuilliere@gmail.com>
2025-09-15 10:52:09 +0200

    4 ft

---
+++ python-classes/4-square.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+"""
+Module that defines an empty Square class.
+"""
+
+
+class Square:
+    """
+    Square class empty with private size attribute
+    """
+
+    def __init__(self, size=0):
+        self.__size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        return self.__size**2
