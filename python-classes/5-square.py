d6f2cb823f5be05d47f18086754557b99031e541
mAgAtSuRi <tristan.latuilliere@gmail.com>
2025-09-15 10:59:33 +0200

    if size = 0added

--- python-classes/5-square.py
+++ python-classes/5-square.py
@@ -29,6 +29,8 @@ class Square:
         return self.__size**2
 
     def my_print(self):
+        if self.size == 0:
+            print()
         for i in range(self.size):
             for j in range(self.size):
                 print("#", end="")
