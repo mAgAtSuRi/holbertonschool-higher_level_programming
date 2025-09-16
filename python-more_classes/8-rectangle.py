5a7facdd9128524c01a4c3cb4961b07598cc6e9b
Tristan <tristan.latuilliere@gmail.com>
2025-09-16 14:48:03 +0200

    static method added

--- python-more_classes/8-rectangle.py
+++ python-more_classes/8-rectangle.py
@@ -67,6 +67,7 @@ class Rectangle:
         Rectangle.number_of_instances -= 1
         print("Bye rectangle...")
 
+    @staticmethod
     def bigger_or_equal(rect_1, rect_2):
         if not isinstance(rect_1, Rectangle):
             raise TypeError("rect_1 must be an instance of Rectangle")
@@ -74,4 +75,4 @@ class Rectangle:
             raise TypeError("rect_2 must be an instance of Rectangle")
         if rect_1.area() == rect_2.area():
             return rect_1
-        return rect_1.area if rect_1.area() > rect_2.area() else rect_2
+        return rect_1 if rect_1.area() > rect_2.area() else rect_2
