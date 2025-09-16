6e669bd89c0cb8b5a4420b495c5e15da716ab907
Tristan <tristan.latuilliere@gmail.com>
2025-09-16 11:19:46 +0200

    formatted

--- python-more_classes/6-rectangle.py
+++ python-more_classes/6-rectangle.py
@@ -62,5 +62,5 @@ class Rectangle:
         return f"Rectangle({self.width}, {self.height})"
 
     def __del__(self):
-        Rectangle.number_of_instance -= 1
+        Rectangle.number_of_instances -= 1
         print("Bye rectangle...")
