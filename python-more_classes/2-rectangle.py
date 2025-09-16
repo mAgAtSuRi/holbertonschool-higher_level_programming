9a3a47e7ac3df63945458e873478340204266da1
Tristan <tristan.latuilliere@gmail.com>
2025-09-16 10:45:08 +0200

    perimeter returns  0 if height or width = 0

--- python-more_classes/2-rectangle.py
+++ python-more_classes/2-rectangle.py
@@ -42,4 +42,7 @@ class Rectangle:
         return self.width * self.height
 
     def perimeter(self):
+        if self.width == 0 or self.height == 0:
+            return 0
+
         return 2 * self.width + 2 * self.height
