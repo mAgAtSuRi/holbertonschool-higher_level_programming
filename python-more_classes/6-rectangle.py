663520ecd825617dd8855f540832ca110ce7b0ce
Tristan <tristan.latuilliere@gmail.com>
2025-09-16 11:16:38 +0200

    Rectangle instead of self because class variable

--- python-more_classes/6-rectangle.py
+++ python-more_classes/6-rectangle.py
@@ -14,7 +14,7 @@ class Rectangle:
     def __init__(self, width=0, height=0):
         self.width = width
         self.height = height
-        self.number_of_instance += 1
+        Rectangle.number_of_instance += 1
 
     @property
     def width(self):
