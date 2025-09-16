84290265e0c4dc1d27b51c88ee3f82ee45c7eca9
Tristan <tristan.latuilliere@gmail.com>
2025-09-16 10:06:30 +0200

    0 ft

---
+++ python-test_driven_development/python-more_classes/0-rectangle.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+"""
+Module with Rectangle class that prints a rectangle
+Empty for now
+"""
+
+
+class Rectangle:
+    """
+    Rectangle class that prints a rectangle
+    """
+
+    pass
