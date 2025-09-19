0934a4274b56ee9bc0dee2320574735ccf238766
Tristan <tristan.latuilliere@gmail.com>
2025-09-19 10:54:35 +0200

    101 FT

--- python-more_classes/101-nqueens.py
+++ python-more_classes/101-nqueens.py
@@ -3,12 +3,11 @@ import sys
 
 
 class Nqueens():
-	queen_list = []
 
 	def __init__(self, size):
-		self.size = int(size)
-		self.board = self.create_board()
-		self.new_pos = []
+		self.size = size
+		self.solutions = set()
+		self.run()
 
 	def create_board(self):
 		board = []
@@ -17,46 +16,56 @@ class Nqueens():
 				board.append([i, j])
 		return board
 
-	def remove_attacked_square(self):
-		queen_position = self.board[0]
-		self.queen_list.append(queen_position)
+	def remove_attacked_square(self, board, queen_position):
+		new_pos = [pos for pos in board]
 
-		self.new_pos = [pos for pos in self.board]
 		for k in range(self.size):
 			# top right diagonal
 			tr_new_x = queen_position[0] + k
 			tr_new_y = queen_position[1] + k
-			if tr_new_x < self.size and tr_new_y < self.size:
-				self.new_pos.remove([tr_new_x, tr_new_y])
+			if [tr_new_x, tr_new_y] in new_pos:
+				new_pos.remove([tr_new_x, tr_new_y])
 			# top left diagonal
 			tl_new_x = queen_position[0] - k
 			tl_new_y = queen_position[1] + k
-			if [tl_new_x, tl_new_y] in self.new_pos:
-				self.new_pos.remove([tl_new_x, tl_new_y])
+			if [tl_new_x, tl_new_y] in new_pos:
+				new_pos.remove([tl_new_x, tl_new_y])
 			# bottom right diagonal
 			br_new_x = queen_position[0] + k
 			br_new_y = queen_position[1] - k
-			if [br_new_x, br_new_y] in self.new_pos:
-				self.new_pos.remove([br_new_x, br_new_y])
+			if [br_new_x, br_new_y] in new_pos:
+				new_pos.remove([br_new_x, br_new_y])
 			# bottom left diagonal
 			bl_new_x = queen_position[0] - k
 			bl_new_y = queen_position[1] - k
-			if [bl_new_x, bl_new_y] in self.new_pos:
-				self.new_pos.remove([bl_new_x, bl_new_y])
+			if [bl_new_x, bl_new_y] in new_pos:
+				new_pos.remove([bl_new_x, bl_new_y])
 
 		for i in range(self.size):
 			for j in range(self.size):
 				# same row or column
 				if i == queen_position[0] or j == queen_position[1]:
-					if [i, j] in self.new_pos:
-						self.new_pos.remove([i, j])
-		self.board = self.new_pos
-		return self.board
+					if [i, j] in new_pos:
+						new_pos.remove([i, j])
+
+		return new_pos
+
+	def solve(self, board, queen_list=[]):
+		if len(queen_list) == self.size:
+			set_solutions = tuple(sorted(tuple(q) for q in queen_list))
+			self.solutions.add(set_solutions)
+			return self.solutions
+	
+		for queen_pos in board:
+			new_board = self.remove_attacked_square(board, queen_pos)
+			self.solve(new_board, queen_list + [queen_pos])
+
+	def run(self):
+		self.solve(self.create_board())
+		return self.solutions
 
 	def __str__(self):
-		return str(self.remove_attacked_square())
-
-
+		return '\n'.join(str(list(map(list, sol))) for sol in self.solutions)
 if __name__ == "__main__":
 
 	if len(sys.argv) != 2:
