7a1ce9d07f16fa2843ddda22460829174221f0ac
mAgAtSuRi <tristan.latuilliere@gmail.com>
2025-09-22 12:28:27 +0200

    6 ft

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+module geometry
+"""
+
+
+class BaseGeometry:
+    """
+    create a BaseGeometry class raising an exception
+    """
+
+    def area(self):
+        raise Exception("area() is not implemented")
