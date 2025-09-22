e2560f1c90e7cf7af5d0cc0f1eddfa3a5b4525de
mAgAtSuRi <tristan.latuilliere@gmail.com>
2025-09-22 10:29:28 +0200

    doc

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -6,4 +6,7 @@ function that returns the list of attributes and methos of a class
 
 
 def lookup(obj):
+    """
+    returns the list of attributes and methods of a class
+    """
     return dir(obj)
