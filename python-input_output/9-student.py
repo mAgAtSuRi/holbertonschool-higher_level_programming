e477368cfa43e1154021a1975a0846f6b9c845eb
mAgAtSuRi <tristan.latuilliere@gmail.com>
2025-09-29 15:35:21 +0200

    dic of instance and not class

--- python-input_output/9-student.py
+++ python-input_output/9-student.py
@@ -11,4 +11,14 @@ class Student:
         self.age = age
 
     def to_json(self):
-        return Student.__dict__
+        return self.__dict__
+
+students = [Student("John", "Doe", 23), Student("Bob", "Dylan", 27)]
+
+for student in students:
+    j_student = student.to_json()
+    print(j_student)
+    # print(j_student['first_name'])
+    # print(type(j_student['first_name']))
+    # print(j_student['age'])
+    # print(type(j_student['age']))
