494a9868c566eeb36cf5035ac7177a2a1f98be34
mAgAtSuRi <tristan.latuilliere@gmail.com>
2025-09-29 09:39:44 +0200

    test with for loop

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -8,4 +8,7 @@ Module to read a file
 def read_file(filename=""):
     """Function reading a file"""
     with open(filename) as f:
-        print(f.read())
+        for line in f:
+            print(line, end="")
+
+read_file("my_file_0.txt")
