4f059c4102fae7cd53b256b00acf4475b607c54d
mAgAtSuRi <tristan.latuilliere@gmail.com>
2025-09-29 10:44:08 +0200

    7 ft

---
+++ python-input_output/7-add_item.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+"""Module to load add and save with json"""
+import sys
+
+save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
+load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
+
+
+arg_list = []
+for i in range(1, len(sys.argv)):
+    arg_list.append(sys.argv[i])
+
+save_to_json_file(arg_list, "add_item.json")
