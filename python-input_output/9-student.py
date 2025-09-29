cc3dcdabb6d5a6fa3156cfd2e641834d93f5631b
mAgAtSuRi <tristan.latuilliere@gmail.com>
2025-09-29 15:35:36 +0200

    formatted

--- python-input_output/9-student.py
+++ python-input_output/9-student.py
@@ -5,6 +5,7 @@
 
 class Student:
     """Student class"""
+
     def __init__(self, first_name, last_name, age):
         self.first_name = first_name
         self.last_name = last_name
@@ -12,13 +13,3 @@ class Student:
 
     def to_json(self):
         return self.__dict__
-
-students = [Student("John", "Doe", 23), Student("Bob", "Dylan", 27)]
-
-for student in students:
-    j_student = student.to_json()
-    print(j_student)
-    # print(j_student['first_name'])
-    # print(type(j_student['first_name']))
-    # print(j_student['age'])
-    # print(type(j_student['age']))
