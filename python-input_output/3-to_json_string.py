e0803b2df6ba22a3c60ea02adfa30054c3aaacf6
mAgAtSuRi <tristan.latuilliere@gmail.com>
2025-09-29 09:57:01 +0200

    3 ft

---
+++ python-input_output/3-to_json_string.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+"""Module returning a JSOn object"""
+import json
+
+
+def to_json_string(my_obj):
+    """FUnction returning an obj in json format"""
+    return json.dumps(my_obj)
