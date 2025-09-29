65e5f79df313ff5b9dbd59fc7e38dfec80880000
mAgAtSuRi <tristan.latuilliere@gmail.com>
2025-09-29 16:32:30 +0200

    saving

---
+++ python-input_output/11-student.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+"""Module to write a student class"""
+
+
+class Student:
+    """Student class"""
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if isinstance(attrs, list) and all(isinstance(elem, str) for
+                                           elem in attrs):
+            return {k: v for k, v in self.__dict__.items() if k in attrs}
+        return self.__dict__
+
+    def reload_from_json(self, json):
+        
