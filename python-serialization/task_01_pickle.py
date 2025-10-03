d2a8232a76d349bcbbfc49d4a0e65cdb53a1a74e
Tristan <tristan.latuilliere@gmail.com>
2025-10-03 11:07:14 +0200

    formatted

--- python-serialization/task_01_pickle.py
+++ python-serialization/task_01_pickle.py
@@ -13,9 +13,9 @@ class CustomObject:
         self.is_student = is_student
 
     def display(self):
-        dic = {"Name:": self.name, "Age:": self.age, "Is Student:": self.is_student}
-        for k, v in dic.items():
-            print(f"{k} {v}")
+        print(f"Name: {self.name}")
+        print(f"Age: {self.age}")
+        print(f"Is Student: {self.is_student}")
 
     def serialize(self, filename):
         with open(filename, "wb") as f:
@@ -23,5 +23,17 @@ class CustomObject:
 
     @classmethod
     def deserialize(cls, filename):
-        with open(filename, "rb") as f:
+        with open(filename, 'rb') as f:
             return pickle.load(f)
+
+obj = CustomObject(name="John", age=25, is_student=True)
+print("Original Object:")
+obj.display()
+
+# Serialize the object
+obj.serialize("object.pkl")
+
+# Deserialize the object into a new instance
+new_obj = CustomObject.deserialize("object.pkl")
+print("\nDeserialized Object:")
+new_obj.display()
