5917772a2089b5b89b7336f6274574c93f6d06a0
Tristan <tristan.latuilliere@gmail.com>
2025-10-07 16:28:02 +0200

    4 ft

---
+++ restful-api/task_04_flask.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+	return "<p>Welcome to the Flask API!</p>"
+
+users = {"jane": {"name": "Jane", "age": 28, "city": "Los Angeles"}}
+
+@app.route("/data")
+def json_function():
+	return jsonify(users)
+
+@app.route("/status")
+def status_function():
+	return "OK"
+
+@app.route("/users/<username>")
+def dynamic_content_func(username):
+	if username in users:
+		return jsonify(users[username])
+	else:
+		return jsonify({"error": "User not found"}), 404
+
+@app.route("/add_user", methods=["POST"])
+def add_user():
+	data = request.get_json()
+
+	if "username" not in data:
+		return jsonify({"error": "Username is required"}), 400
+	
+	name = data["username"]
+	users[name] = data
+	message = {
+		"message": f"User {name} added",
+		"user": data
+	}
+	return jsonify(message), 201
+
+if __name__ == "__main__": app.run()
