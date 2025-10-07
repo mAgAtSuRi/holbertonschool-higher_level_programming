e6648b37cf99618fecfca3bfed7c66003bf12b19
Tristan <tristan.latuilliere@gmail.com>
2025-10-07 14:37:38 +0200

    changed else to another response instead of senderror

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -15,7 +15,7 @@ class MyHandler(BaseHTTPRequestHandler):
         elif self.path == "/data":
             data = {"name": "John", "age": 30, "city": "New York"}
             self.send_response(200)
-            self.send_header("Content-type", "application/JSON")
+            self.send_header("Content-type", "application/json")
             self.end_headers()
             self.wfile.write(json.dumps(data).encode())
 
@@ -26,7 +26,10 @@ class MyHandler(BaseHTTPRequestHandler):
             self.wfile.write(b"OK")
 
         else:
-            self.send_error(404, "undefined endpoint")
+            self.send_response(404)
+            self.send_header("Content-type", "text/plain")
+            self.end_headers()
+            self.wfile.write("Undefined Endpoint")
 
 
 server_adress = ("", 8000)
