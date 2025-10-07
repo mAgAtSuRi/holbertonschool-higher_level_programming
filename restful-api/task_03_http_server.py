9fd885404a0f751d34573c082b57f044be2b1838
Tristan <tristan.latuilliere@gmail.com>
2025-10-07 14:28:46 +0200

    3 ft

---
+++ restful-api/task_03_http_server.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import json
+
+
+class MyHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        if self.path == "/":
+            self.send_response(200)
+            self.send_header("Content-type", "text/plain")
+            self.end_headers()
+            self.wfile.write(b"Hello, this is a simple API!")
+
+        elif self.path == "/data":
+            data = {"name": "John", "age": 30, "city": "New York"}
+            self.send_response(200)
+            self.send_header("Content-type", "application/JSON")
+            self.end_headers()
+            self.wfile.write(json.dumps(data).encode())
+
+        elif self.path == "/status":
+            self.send_response(200)
+            self.send_header("Content-type", "text/plain")
+            self.end_headers()
+            self.wfile.write(b"OK")
+
+        else:
+            self.send_error(404, "undefined endpoint")
+
+
+server_adress = ("", 8000)
+httpd = HTTPServer(server_adress, MyHandler)
+print("✅ Server running on http://localhost:8000")
+httpd.serve_forever()
