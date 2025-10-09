f7823a208a75da9488749d165dae9816b9c0e824
Tristan <tristan.latuilliere@gmail.com>
2025-10-09 15:41:05 +0200

    error 401 added

--- restful-api/task_05_basic_security.py
+++ restful-api/task_05_basic_security.py
@@ -14,51 +14,74 @@ jwt = JWTManager(app)
 
 users = {
     "user1": {
-		"username": "user1",
-		"password": generate_password_hash("password"),
-		"role": "user"
-		},
+        "username": "user1",
+        "password": generate_password_hash("password"),
+        "role": "user",
+    },
     "admin1": {
-		"username": "admin1",
-		"password": generate_password_hash("password"),
-		"role": "admin"
-		}
+        "username": "admin1",
+        "password": generate_password_hash("password"),
+        "role": "admin",
+    },
 }
 
 
 @auth.verify_password
 def verify_password(username, password):
-	if username in users and check_password_hash(users[username]['password'], password):
-	    return username
+    if username in users and check_password_hash(users[username]["password"], password):
+        return username
 
 
-
-@app.route('/basic-protected')
+@app.route("/basic-protected")
 @auth.login_required
 def index():
-	return "Basic Auth: Access Granted"
+    return "Basic Auth: Access Granted"
 
-@app.route('/login', methods=['POST'])
+
+@app.route("/login", methods=["POST"])
 def login():
     username = request.json.get("username", None)
     password = request.json.get("password", None)
-    if username not in users or not check_password_hash(users[username]['password'], password):
-        return jsonify({'msg': '401 Unauthorized'}), 401	
+    if username not in users or not check_password_hash(
+        users[username]["password"], password
+    ):
+        return jsonify({"msg": "401 Unauthorized"}), 401
     token = create_access_token(identity=username)
     return jsonify(access_token=token), 200
 
-@app.route('/jwt-protected')
+
+@app.route("/jwt-protected")
 @jwt_required()
 def jwt_protected():
-     return "JWT Auth: Access Granted"
+    return "JWT Auth: Access Granted"
+
 
 @app.route("/admin-only")
 @jwt_required()
 def check_admin():
-     current_user = get_jwt_identity()
-     if users[current_user]['role'] != 'admin':
-          return jsonify({"error": "Admin access required"}), 403
-     return "Admin Access: Granted"
+    current_user = get_jwt_identity()
+    if users[current_user]["role"] != "admin":
+        return jsonify({"error": "Admin access required"}), 403
+    return "Admin Access: Granted"
 
-if __name__ == '__main__':
+
+# Cas ou le token est absent
+@jwt.unauthorized_loader
+def missing_token_callback(err):
+    return jsonify({"error": "Missing or invalid token"}), 401
+
+
+# Cas ou le token est invalide
+@jwt.invalid_token_loader
+def handle_invalid_token_error(err):
+    return jsonify({"error": "Invalid token"}), 401
+
+
+# Cas ou le token a expiré
+@jwt.expired_token_loader
+def expired_token_callback(err):
+    return jsonify({"error": "Token has expired"}), 401
+
+
+if __name__ == "__main__":
     app.run()
