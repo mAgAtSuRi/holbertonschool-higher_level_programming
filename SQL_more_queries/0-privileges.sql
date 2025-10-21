5defcc9c80a940aa6b3fae3a9393d2d894242faf
Tristan <tristan.latuilliere@gmail.com>
2025-10-21 15:41:56 +0200

    rename

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,3 +1,3 @@
--- lists all privileges of the MySQL users user_0d_1 and user_0d_2 on your server (in localhost).
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+--  lists all privileges of the MySQL users user_0d_1 and user_0d_2 on your server (in localhost).
+SHOW GRANTS FOR 'user_0d_1';
+SHOW GRANTS FOR 'user_0d_2';
