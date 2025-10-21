80ba25af63bf44b235b1fc52a857b23e71f722eb
Tristan <tristan.latuilliere@gmail.com>
2025-10-21 15:53:59 +0200

    2 ft

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -1,3 +1,3 @@
 --  lists all privileges of the MySQL users user_0d_1 and user_0d_2 on your server (in localhost).
-SHOW GRANTS FOR 'user_0d_1';
-SHOW GRANTS FOR 'user_0d_2';
+CREATE IF NOT EXISTS DATABASE hbtn_0d_2;
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhots';
