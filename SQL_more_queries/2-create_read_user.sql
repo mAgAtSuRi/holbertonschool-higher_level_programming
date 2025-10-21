0a1867a2fbdda8450fe6f938a63724ad25f12c3f
Tristan <tristan.latuilliere@gmail.com>
2025-10-21 16:00:07 +0200

    formatted

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -1,4 +1,4 @@
 --  lists all privileges of the MySQL users user_0d_1 and user_0d_2 on your server (in localhost).
-CREATE IF NOT EXISTS DATABASE hbtn_0d_2;
-CREATE USER IF NOT EXISTS 'user_0d_2'@'localhots';
-GRANT SELECT PRIVILEGE ON 'hbtn_0d_2' to 'user_0d_2'@'localhots';2 
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost';
+GRANT SELECT ON hbtn_0d_2.* to 'user_0d_2'@'localhots';
