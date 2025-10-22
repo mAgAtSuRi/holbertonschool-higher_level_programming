92af0296e1dea812931dfaaa4e17ac5ba0e82b4d
mAgAtSuRi <tristan.latuilliere@gmail.com>
2025-10-22 19:44:26 +0200

    asc genre

--- SQL_more_queries/14-my_genres.sql
+++ SQL_more_queries/14-my_genres.sql
@@ -6,4 +6,4 @@ ON tv_shows.id = tv_show_genres.show_id
 INNER JOIN tv_genres
 ON tv_genres.id = tv_show_genres.genre_id
 WHERE tv_shows.title = "Dexter"
-ORDER BY tv_shows.title ASC;
+ORDER BY tv_genres.name ASC;
