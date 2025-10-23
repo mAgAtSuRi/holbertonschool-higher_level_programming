1a4b830d9d2eab028983dad4e15b161dfe0e8966
Tristan <tristan.latuilliere@gmail.com>
2025-10-23 16:06:16 +0200

    formatted

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -9,19 +9,22 @@ if __name__ == "__main__":
     password = sys.argv[2]
     db_name = sys.argv[3]
     state_name = sys.argv[4]
-    
+
     db = MySQLdb.connect(
         host="localhost", port=3306, user=user, password=password, db=db_name
     )
     cursor = db.cursor()
-    cursor.execute("""
+    cursor.execute(
+        """
         SELECT cities.name
         FROM cities
         INNER JOIN states ON cities.state_id = states.id
         WHERE states.name = %s
         ORDER BY cities.name ASC
-    """, (state_name,))
-    
+    """,
+        (state_name,),
+    )
+
     rows = cursor.fetchall()
     for row in rows:
         print(row)
