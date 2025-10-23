5ca543c77e2e188d3fa04feef6d7b46ab9eb3361
Tristan <tristan.latuilliere@gmail.com>
2025-10-23 16:07:33 +0200

    LIKE BINARY

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -19,7 +19,7 @@ if __name__ == "__main__":
         SELECT cities.name
         FROM cities
         INNER JOIN states ON cities.state_id = states.id
-        WHERE states.name = %s
+        WHERE states.name LIKE BINARY %s
         ORDER BY cities.name ASC
     """,
         (state_name,),
