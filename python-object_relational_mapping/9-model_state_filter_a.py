f39330611d763ba20bb418df2bd1cd69486b3c9c
mAgAtSuRi <tristan.latuilliere@gmail.com>
2025-10-24 12:19:35 +0200

    format

--- python-object_relational_mapping/9-model_state_filter_a.py
+++ python-object_relational_mapping/9-model_state_filter_a.py
@@ -14,8 +14,7 @@ if __name__ == "__main__":
     password = sys.argv[2]
     db = sys.argv[3]
 
-    engine = create_engine(f"mysql+mysqldb://
-                           {user}:{password}@localhost:3306/{db}")
+    engine = create_engine(f"mysql+mysqldb://{user}:{password}@localhost:3306/{db}")
 
     Session = sessionmaker(bind=engine)
     session = Session()
