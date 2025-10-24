64a8a3582e42bcb1f2bec3a2dbeefa192e682e83
mAgAtSuRi <tristan.latuilliere@gmail.com>
2025-10-24 13:55:05 +0200

    delph

--- python-object_relational_mapping/13-model_state_delete_a.py
+++ python-object_relational_mapping/13-model_state_delete_a.py
@@ -1,26 +1,48 @@
 #!/usr/bin/python3
-
+"""
+Script that updates a state in the 'state' table from the database
+hbtn_0e_6_usa
+"""
 import sys
-from model_state import State
-from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+from model_state import Base, State
+
 
 if __name__ == "__main__":
-    user, password, db = sys.argv[1], sys.argv[2], sys.argv[3]
+    # Validate the number of arguments passed
+    if len(sys.argv) != 4:
+        print("Usage: {} <user> <password> <db_name>".format(sys.argv[0]))
+        sys.exit(1)
 
-    engine = create_engine(
-        f"mysql+mysqldb://{user}:{password}@localhost:3306/{db}",
-        pool_pre_ping=True
-    )
+    # Get database connection details from command-line arguments
+    user = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
 
+    # Construct the database URL for SQLAlchemy
+    DB_URL = f"mysql+mysqldb://{user}:{password}@localhost:3306/{db_name}"
+
+    # Create the SQLAlchemy engine to manage database connections
+    engine = create_engine(DB_URL, pool_pre_ping=True)
+
+    # Create all tables defined in Base's metadata (e.g., 'states' table)
+    # This ensures the table exists in the database
+    Base.metadata.create_all(engine)
+
+    # Create a configured Session class
     Session = sessionmaker(bind=engine)
+
+    # Create a new Session instance
     session = Session()
 
-    states = session.query(State).filter(State.name.like("%a%")).all()
-    
-    if states:
-        for state in states:
-            session.delete(state)
+    # Delete all states containing the letter 'a'
+    delete_state = session.query(State).filter(State.name.like('%a%')).all()
+
+    if delete_state:
+        for erase in delete_state:
+            session.delete(erase)
         session.commit()
 
+    # Close the session to release database resources
     session.close()
