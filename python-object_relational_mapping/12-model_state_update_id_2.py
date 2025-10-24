e0d903d4b5c282f1e640e3c5611f0b9fc6b14b5f
mAgAtSuRi <tristan.latuilliere@gmail.com>
2025-10-24 13:24:28 +0200

    12 ft

---
+++ python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+import sys
+from model_state import State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+    user, password, db = sys.argv[1], sys.argv[2], sys.argv[3]
+
+    engine = create_engine(
+        f"mysql+mysqldb://{user}:{password}@localhost:3306/{db}",
+        pool_pre_ping=True
+    )
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    state = session.query(State).filter(State.id == 2).first()
+    state.name = "New Mexico"
+    session.commit()
