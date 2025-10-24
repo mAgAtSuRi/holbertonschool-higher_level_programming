cb48d9c2704a2acc850d57385ae23dfa5bba26fe
mAgAtSuRi <tristan.latuilliere@gmail.com>
2025-10-24 11:43:58 +0200

    format

--- python-object_relational_mapping/8-model_state_fetch_first.py
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,18 +1,20 @@
 #!/usr/bin/python3
+"""
+Prints the first State object from the database hbtn_0e_6_usa
+"""
 
-from model_state import Base, State
 import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
 
 if __name__ == "__main__":
     # if len(sys.argv) != 4:
     #     sys.exit(1)
 
-    user = sys.argv[1]
-    password = sys.argv[2]
-    db = sys.argv[3]
+    user, password, db = sys.argv[1], sys.argv[2], sys.argv[3]
 
+    # Connexion à la base
     engine = create_engine(
         f"mysql+mysqldb://{user}:{password}@localhost:3306/{db}",
         pool_pre_ping=True
@@ -21,10 +23,12 @@ if __name__ == "__main__":
     Session = sessionmaker(bind=engine)
     session = Session()
 
+    # Récupère uniquement le premier State trié par id
     first_state = session.query(State).order_by(State.id).first()
-    if first_state:
-        print(f"{first_state.id}: {first_state.name}")
-    else:
+
+    if first_state is None:
         print("Nothing")
+    else:
+        print(f"{first_state.id}: {first_state.name}")
 
     session.close()
