64e9d3a626daf58e4f61e1edf46284229457d416
mAgAtSuRi <tristan.latuilliere@gmail.com>
2025-11-09 15:15:04 +0100

    space removed

--- javascript-warm_up/6-multi_languages_loop.js
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const  argv  = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+const argv = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 let i = 0;
 while (argv[i]) {
   console.log(`${argv[i]}`);
