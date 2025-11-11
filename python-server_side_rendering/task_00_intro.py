1f66ba02a69a09b68af5c9e1e280548131acb92a
mAgAtSuRi <tristan.latuilliere@gmail.com>
2025-11-11 10:40:31 +0100

    0 ft

---
+++ python-server_side_rendering/task_00_intro.py
@@ -0,0 +1,19 @@
+def generate_invitations(template, attendees):
+    if not isinstance(template, str) or not (isinstance(attendees, list) and all(isinstance(a, dict) for a in attendees)):
+        raise TypeError("template must be a string and attendees a list of dictionaries")
+
+    if not template.strip():
+        raise ValueError("Template is empty, no output files generated.")
+    if len(attendees) == 0:
+        raise ValueError("No data provided, no output files generated.")
+
+    for i in range(len(attendees)):
+        invitation = template.replace("{name}", attendees[i].get("name") or "N/A") \
+                                .replace("{event_title}", attendees[i].get("event_title") or "N/A") \
+                                .replace("{event_date}", attendees[i].get("event_date") or "N/A") \
+                                .replace("{event_location}", attendees[i].get("event_location") or "N/A")
+        try:
+            with open(f"output_{i + 1}.txt", 'w') as f:
+                f.write(invitation)
+        except OSError as e:
+            print(f"Couldn't write the invitation file {i}. Error: {e}")
